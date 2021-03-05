b600ecbba5b723b8768f37c450c45ac6b0b091e4
michelchalfoun <mikachalfoun@gmail.com>
2021-03-05 00:53:19 -0500

    Added basic pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { Route, Switch, Redirect  } from 'react-router-dom';
-import Home from "./views/Home/Home"
-import NotFound from "./views/NotFound"
-import Header from "./components/Header/Header"
-
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import DashboardPage from "./views/DashboardPage/DashboardPage";
+import NotFound from "./views/NotFound";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Switch>
-        <Route exact path="/Home" component={Home} />
+        <Route exact path="/Dashboard" component={DashboardPage} />
         <Route exact path="/">
-          <Redirect to="/Home" />
+          <Redirect to="/Dashboard" />
         </Route>
-        <Route component={NotFound}/>
+        <Route component={NotFound} />
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
