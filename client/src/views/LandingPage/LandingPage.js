e981ae1902d17cf4eda6cf7ab8770e99c949ac84
Michel Chalfoun <mikachalfoun@gmail.com>
2021-03-16 14:49:55 -0400

    Made landing page a component

--- client/src/views/LandingPage/LandingPage.js
+++ client/src/views/LandingPage/LandingPage.js
@@ -1,22 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "../../assets/logo.svg";
 import "./LandingPage.css";
 
-function LandingPage() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>This is the landing page.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        ></a>
-      </header>
-    </div>
-  );
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>This is the landing page.</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          ></a>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default LandingPage;
