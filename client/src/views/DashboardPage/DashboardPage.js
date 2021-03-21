91837f04c22ecc4ad52783dd43166355a49a1aab
Michaela <m.isaacs@ufl.edu>
2021-03-21 00:39:53 -0400

    basic events agenda

--- client/src/views/DashboardPage/DashboardPage.js
+++ client/src/views/DashboardPage/DashboardPage.js
@@ -2,22 +2,13 @@ import React from "react";
 import logo from "../../assets/logo.svg";
 import "./DashboardPage.scss";
 import Header from "../../components/Header/Header";
+import UpcomingEvents from "../../components/Calender/UpcomingEvents.js";
+
 
 function DashboardPage() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        ></a>
-      </header>
+      <UpcomingEvents />
     </div>
   );
 }
