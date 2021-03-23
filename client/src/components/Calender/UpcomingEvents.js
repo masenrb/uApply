710404a7cb4274bbf9d9d40bd3f8226d74625c72
Michel Chalfoun <mikachalfoun@gmail.com>
2021-03-23 05:16:17 -0400

    clean up

--- client/src/components/Calender/UpcomingEvents.js
+++ client/src/components/Calender/UpcomingEvents.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Card, Button, Segment, Header } from "semantic-ui-react";
 import "./UpcomingEvents.scss";
 
